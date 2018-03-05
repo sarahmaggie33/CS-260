f8c0b51ddf5620587fcabd8aab02ce56911e992c
sarahmaggie33 <sarahericson33@gmail.com>
2018-03-05 15:57:19 -0600

    with the GridBagConstants... changing the layout now...

--- CS 260 Assignment 1/src/gui/Henry.java
+++ CS 260 Assignment 1/src/gui/Henry.java
@@ -1,14 +1,17 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.border.Border;
 
 public class Henry {
 	public static void main(String[] args) {
@@ -17,20 +20,30 @@ public class Henry {
 //		Toolkit.getDefaultToolkit().getScreenResolution(); 
 		int jFrameWidth = (int) (screenDimension.width * 0.4);
 		int jFrameHeight = (int) (screenDimension.height * 0.5);
-		f.setSize(jFrameWidth, jFrameHeight);
+		f.setBounds(0, 0, jFrameWidth, jFrameHeight);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		
+        
 		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.setSize(jFrameWidth, jFrameHeight);
+		tabbedPane.setBounds(0, 0, jFrameWidth, jFrameHeight);
+		
+//		Tab 1
 		JPanel author = new JPanel();
 		author.add(SearchByAuthorPanel.author());
 		tabbedPane.addTab("Search by Author", author);
-		tabbedPane.setSelectedIndex(0);
-//		tabbedPane.addTab("Search by ");
-
+//		tabbedPane.setSelectedIndex(0);
 		
+//		Tab 2
+		JPanel category = new JPanel();
+		JPanel categoryContent = new JPanel(new BorderLayout());
+		category.add(categoryContent);
+		tabbedPane.addTab("Search by Category", category);
+
+//		tabbedPane.setOpaque(true);
 		tabbedPane.setFont(new Font("Serif", Font.BOLD, 50));
 		f.add(tabbedPane);
-		
+//		f.pack();
 		f.setVisible(true);
 	}
 }
--- CS 260 Assignment 1/src/gui/SearchByAuthorPanel.java
+++ CS 260 Assignment 1/src/gui/SearchByAuthorPanel.java
@@ -2,38 +2,34 @@ package gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
 public class SearchByAuthorPanel extends JPanel{
 	@SuppressWarnings("unchecked")
 	public static JPanel author() {
 		
-//		// Create the frame
-//		JFrame f = new JFrame();
-//		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
-////		Toolkit.getDefaultToolkit().getScreenResolution() 
-//		int jFrameWidth = (int) (screenDimension.width * 0.4);
-//		int jFrameHeight = (int) (screenDimension.height * 0.5);
-//		f.setSize(jFrameWidth, jFrameHeight);
-//		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Make the x button kill Java
-		
 		// Make a panel to hold our content and put it on the frame
 		JPanel mainPanel = new JPanel();
+//		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+//		int panelWidth = (int) (screenDimension.width * 0.4);
+//		int panelHeight = (int) (screenDimension.height * 0.5);
+//		mainPanel.setSize(panelWidth, panelHeight);
 		mainPanel.setBackground(Color.CYAN);
-//		f.setContentPane(mainPanel);
 		
 		// Make author dropdown
 		String[] author = {"author1", "author2", "author3"};
 		JComboBox authorDropDown = new JComboBox(author);
 		authorDropDown.setFont(new Font("Serif", Font.PLAIN, 50));
-		mainPanel.add(authorDropDown);
+		
 		
 		// Add a listener to the button so we can do something when it is clicked
 		authorDropDown.addActionListener(new ActionListener() {
@@ -42,35 +38,74 @@ public class SearchByAuthorPanel extends JPanel{
 				// This will be called when the button is clicked
 				JComboBox combo = (JComboBox)e.getSource();
 				String selection = (String)combo.getSelectedItem();
-				System.out.println("author selected: " + selection);
-				
+				System.out.println("author selected: " + selection);	
 			}
 			
 		});
 		
 			
 		// Make bookstore dropdown
-		String[] bookstore = {"store1", "store2", "store3"};
-		JComboBox storeDropDown = new JComboBox(bookstore);
-		storeDropDown.setFont(new Font("Serif", Font.PLAIN, 50));
-		mainPanel.add(storeDropDown);
+		String[] book = {"book1", "book2", "book3"};
+		JComboBox bookDropDown = new JComboBox(book);
+		bookDropDown.setFont(new Font("Serif", Font.PLAIN, 50));
+		
 		
 		// Add a listener to the button so we can do something when it is clicked
-		storeDropDown.addActionListener(new ActionListener() {
+		bookDropDown.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// This will be called when the button is clicked
 				JComboBox combo = (JComboBox)e.getSource();
 				String selection = (String)combo.getSelectedItem();
-				System.out.println("Store selected: " + selection);
+				System.out.println("Book selected: " + selection);
 			}			
 		});		
 		
+//		Print out the cost of the book in a text box
 		double p = 12.75;
 		JTextField price = new JTextField("$" + p);
 		price.setFont(new Font("Serif", Font.PLAIN, 50));
-		mainPanel.add(price);
 		
+		
+//		Print out the bookstore and number of books available
+		String[] columnNames = {"Bookstore", "# Books Available"};
+		Object[][] data = {{"Store1", new Integer(1)},{"Store2",new Integer(2)}, {"Store3", new Integer(3)}};
+		JTable table = new JTable(data, columnNames);
+		table.setFont(new Font("Serif", Font.PLAIN, 50));
+		
+		
+//		Layout of the JPanel
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		mainPanel.setLayout(layout);
+
+		
+// 		Add the components to the JPanel
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		mainPanel.add(authorDropDown, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 0;
+		mainPanel.add(bookDropDown, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 0;
+		mainPanel.add(price, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.ipady = 40;     
+		c.weightx = 0.0;
+		c.gridwidth = 3;
+		c.gridx = 0;
+		c.gridy = 1;
+		mainPanel.add(table, c);
 		
 		// Make the frame visible and layout the components on it
 		// Do this last
@@ -78,8 +113,5 @@ public class SearchByAuthorPanel extends JPanel{
 		return mainPanel;
 	}
 
-//	public static void main(String[] args) {
-//		new SearchByAuthorPanel();
-//	}
 	
 }
